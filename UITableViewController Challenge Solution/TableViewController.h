16eb619399ccc58a55dfc7aab1279c49ac2f3ebd
Bita Sobhani <bitasobhani@Bitas-MacBook-Pro.local>
2017-11-29 13:35:36 +0100

    solved the challenge

--- UITableViewController Challenge Solution/TableViewController.h
+++ UITableViewController Challenge Solution/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Bita Sobhani on 29.11.17.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
